5c7b1e59efcd8e166e2b56a21295fdc93ff19592
munishsabharwal <munish.sabharwal@gmail.com>
2026-02-13 22:32:46 -0500

    Update index.js

--- api/LibraryManager/index.js
+++ api/LibraryManager/index.js
@@ -1,6 +1,33 @@
+const { CosmosClient } = require("@azure/cosmos");
+
 module.exports = async function (context, req) {
-    context.res = {
-        status 200,
-        body LibraryManager API is Online
-    };
+    try {
+        const connectionString = process.env.COSMOS_DB_CONNECTION_STRING;
+        const client = new CosmosClient(connectionString);
+        const container = client.database("VaakDb").container("Library");
+
+        if (req.method === "GET") {
+            // SIMPLEST QUERY POSSIBLE
+            const querySpec = {
+                query: "SELECT TOP 50 * FROM c"
+            };
+
+            const { resources: items } = await container.items.query(querySpec).fetchAll();
+            
+            context.res = {
+                status: 200,
+                headers: { "Content-Type": "application/json" },
+                body: items
+            };
+        } else {
+            context.res = { status: 200, body: "Method not supported in this test" };
+        }
+    } catch (err) {
+        context.log.error(err);
+        context.res = {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: err.message })
+        };
+    }
 };
