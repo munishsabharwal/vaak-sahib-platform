fb21a38e750f428187dc0a3d9ed9b0f2bfb2dd74
munishsabharwal <munish.sabharwal@gmail.com>
2026-02-14 14:21:15 -0500

    Update app.js saveEditor element id fix

--- src/app.js
+++ src/app.js
@@ -168,11 +168,12 @@ async function publishVaak(event, item) {
 // SUPER ADMIN: Add Editor
 async function saveEditor() {
 const editorData = {
-        firstName: document.getElementById('editFirstName').value,
-        lastName: document.getElementById('editLastName').value,
-        email: document.getElementById('editEmail').value,
-        gurudwaraName: document.getElementById('editGurudwara').value,
-        gurudwaraLocation: document.getElementById('editLocation').value,
+        firstName: document.getElementById('edFn').value,
+        lastName: document.getElementById('edLn').value,
+        email: document.getElementById('edEmail').value,
+        gurudwaraName: document.getElementById('edGn').value,
+        gurudwaraLocation: document.getElementById('edLoc').value,
+        gurudwaraComment: document.getElementById('edCom').value   
         status: 'Active'
     };
 
